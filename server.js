31a205deae92a8daa723515282bdc7c87fb67a3d
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-02-02 13:34:08 +0100

    transfert

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override');
 const cors = require('cors');
+const mysql = require('mysql');
+
+const session=require('express-session')({
+  secret:"test",
+  resave: true,
+  saveUninitialized: true,
+  cookie:{
+    maxAge: 2*60*60*1000,
+    secure:false
+  }
+});
 
 const account = require('./modules/checkingAccounts.js');
 const mail = require('./modules/sendMail');
@@ -11,6 +22,41 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
+
+if(app.get('env')==='production'){
+  app.set('trust proxy', 1);
+  session.cookie.secure=true;
+}
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'',
+  database: 'test'
+});
+
+con.connect(err=>{
+  let op;
+  if(err){
+    throw err;
+  }else{
+    console.log('Connexion effectuée');
+    op = "INSERT INTO compte (name, password, mail) VALUES ('a', 're', 'paul.lecuisinier921@gmail.com')";
+    con.query(op, (err, result)=>{
+      if(err){
+        throw err;
+      }else{
+        console.log('On a inséré : ')
+        console.log(result);
+      }
+    });
+
+    con.query('SELECT * FROM compte', (err, result)=>{
+      if(err) throw err;
+      console.log(result);
+    })
+  }
+});
 
 app.post('/mailToken', function(req,res){
   mail.sendToken(res, req.body.mail, req.body.name);
